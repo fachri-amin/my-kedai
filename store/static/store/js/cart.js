856ed4166fd0013e869fd2ad153b072b86d19c6e
fachri-amin <fachri.amin@gmail.com>
2020-10-30 21:41:43 +0700

    finish asynchronous

--- store/static/store/js/cart.js
+++ store/static/store/js/cart.js
@@ -1,8 +1,8 @@
-let registerAnchor = document.getElementById('register-anchor')
+let registerAnchor = document.getElementById("register-anchor");
 
-registerAnchor.addEventListener('click', function (e) {
-    e.preventDefault()
-})
+registerAnchor.addEventListener("click", function (e) {
+    e.preventDefault();
+});
 
 let updateBtns = document.getElementsByClassName("update-cart");
 
@@ -12,7 +12,11 @@ for (i = 0; i < updateBtns.length; i++) {
         let action = this.dataset.action;
 
         if (user === "AnonymousUser") {
-            addCookieItem(productId, action, this.dataset.url);
+            if (this.classList.contains("chg-quantity")) {
+                addCookieItem(productId, action, this.dataset.url);
+            } else {
+                addCookieItem(productId, action, this.dataset.url);
+            }
         } else {
             if (this.classList.contains("chg-quantity")) {
                 updateQuantity(productId, action, this.dataset.url, this);
@@ -83,24 +87,40 @@ function addCookieItem(productId, action, url) {
     if (action == "add") {
         if (!cart[productId]) {
             cart[productId] = {
-                'quantity': 1,
+                quantity: 1,
             };
         } else {
             cart[productId]["quantity"] += 1;
+            document.querySelector(`p[data-product='${productId}']`).innerHTML = cart[productId]["quantity"];
         }
     }
 
     if (action == "remove") {
         cart[productId]["quantity"] -= 1;
-        if (cart[productId]["quantity"]) {
+        document.querySelector(`p[data-product='${productId}']`).innerHTML = cart[productId]["quantity"];
+        if (cart[productId]["quantity"] <= 0) {
+            document.querySelector(`div[data-product='${productId}']`).remove();
             delete cart[productId];
         }
     }
 
     document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
 
-    location.reload();
-    // updateUserOrder(productId, action, url);
-    // let cart_total = document.getElementById('cart-total');
-    // cart_total.innerHTML = data.total_item;
+    var total_cart = 0;
+
+    Object.values(cart).forEach(function (item) {
+        total_cart += item.quantity;
+    });
+
+    // console.log(total_cart);
+    let cart_total = document.getElementById("cart-total");
+    try {
+        document.getElementById("cart-alert").classList.remove("hidden");
+        setTimeout(function () {
+            document.getElementById("cart-alert").classList.add("hidden");
+        }, 3000);
+    } catch {}
+    cart_total.innerHTML = total_cart;
+
+    // location.reload();
 }
